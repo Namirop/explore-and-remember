87b28c0e6f0d81203345a1bf198c2ca254fb5644
romai <romainmaes@outlook.fr>
2024-01-13 16:49:48 +0100

    C/P

--- lib/AddLocationPage.dart
+++ lib/AddLocationPage.dart
@@ -74,6 +74,7 @@ class _AddLocationPageState extends State<AddLocationPage> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
